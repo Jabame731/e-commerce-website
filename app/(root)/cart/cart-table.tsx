f85ecb43a5f6b0ed41969bf60b46d2e4747c9390
Jabame731 <justinetaglucop@gmail.com>
2025-06-06 21:49:45 +0800

    shipping-page and check-out

--- app/(root)/cart/cart-table.tsx
+++ app/(root)/cart/cart-table.tsx
@@ -13,8 +13,16 @@ import {
 import Image from "next/image";
 import AddToCartItemButton from "@/components/shared/products/add-item-cart-btn";
 import RemoveItemCartButton from "@/components/shared/products/remove-item-cart-btn";
+import { formatCurrency } from "@/lib/utils";
+import { Card, CardContent } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { useTransition } from "react";
+import { useRouter } from "next/navigation";
+import { ArrowRight } from "lucide-react";
 
 const CartTable = ({ cart }: { cart?: Cart }) => {
+  const router = useRouter();
+  const [isPending, startTransition] = useTransition();
   return (
     <>
       <h1 className="py-4 h2-bold">Shopping Cart</h1>
@@ -65,6 +73,37 @@ const CartTable = ({ cart }: { cart?: Cart }) => {
               </TableBody>
             </Table>
           </div>
+
+          <Card className="p-0">
+            <CardContent className="p-6 flex flex-col gap-6">
+              <div className="text-xl font-medium flex justify-between items-center">
+                <span>
+                  Subtotal (
+                  {cart.items.reduce((acc, item) => acc + item.qty, 0)} item
+                  {cart.items.reduce((acc, item) => acc + item.qty, 0) > 1
+                    ? "s"
+                    : ""}
+                  ):
+                </span>
+                <span className="font-bold text-primary">
+                  {formatCurrency(cart.itemsPrice)}
+                </span>
+              </div>
+
+              <Button
+                className="w-full cursor-pointer flex items-center justify-center gap-2 bg-primary text-white hover:bg-primary/90 transition"
+                disabled={isPending}
+                onClick={() => {
+                  startTransition(() => {
+                    router.push("/shipping-address");
+                  });
+                }}
+              >
+                <ArrowRight className="w-4 h-4" />
+                Proceed to Checkout
+              </Button>
+            </CardContent>
+          </Card>
         </div>
       )}
     </>
