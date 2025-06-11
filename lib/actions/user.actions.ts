722e486ead9586a921431ba3e476800601482dde
Jabame731 <justinetaglucop@gmail.com>
2025-06-11 21:02:08 +0800

    user profile initialize

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -159,3 +159,34 @@ export async function updateUserPaymentMethod(
     return { success: false, message: formatError(error) };
   }
 }
+
+//update user profile
+export async function updateUserProfile(user: { name: string; email: string }) {
+  try {
+    const session = await auth();
+    const currentUser = await prisma.user.findFirst({
+      where: { id: session?.user?.id },
+    });
+
+    if (!currentUser) throw new Error("User not found");
+
+    await prisma.user.update({
+      where: {
+        id: currentUser.id,
+      },
+      data: {
+        name: user.name,
+      },
+    });
+
+    return {
+      success: true,
+      message: "User updated succcessfully",
+    };
+  } catch (error) {
+    return {
+      sucess: false,
+      message: formatError(error),
+    };
+  }
+}
