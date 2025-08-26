573f68fa33c3fa0640dd806a26c44af5065dcc0b
Jabame731 <justinetaglucop@gmail.com>
2025-08-26 16:29:58 +0800

    admin search functionality on orders, products and users

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -6,6 +6,7 @@ import { LATEST_PRODUCTS_LIMIT, PAGE_SIZE } from "../constants";
 import { revalidatePath } from "next/cache";
 import { z } from "zod";
 import { insertProductSchema, updateProductSchema } from "../validators";
+import { Prisma } from "../generated/prisma";
 
 //get latest products
 export async function getLatestProducts() {
@@ -45,7 +46,7 @@ export async function getProductById(productId: string) {
 
 //get all products
 export async function getAllProducts({
-  // query,
+  query,
   limit = PAGE_SIZE,
   page,
 }: // category,
@@ -55,7 +56,20 @@ export async function getAllProducts({
   page: number;
   category?: string;
 }) {
+  const queryFilter: Prisma.ProductWhereInput =
+    query && query !== "all"
+      ? {
+          name: {
+            contains: query,
+            mode: "insensitive",
+          } as Prisma.StringFilter,
+        }
+      : {};
+
   const data = await prisma.product.findMany({
+    where: {
+      ...queryFilter,
+    },
     orderBy: { createdAt: "desc" },
     skip: (page - 1) * limit,
     take: limit,
