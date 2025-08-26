be7ecff7c07a0dee15f89ee8b780c5846037445a
Jabame731 <justinetaglucop@gmail.com>
2025-08-26 17:02:00 +0800

    category-draw

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -3,13 +3,15 @@ import Link from "next/link";
 
 import { APP_NAME } from "@/lib/constants";
 import Menu from "./menu";
+import CategoryDrawer from "./category-draw";
 
 const Header = () => {
   return (
     <header className="w-full border-b">
       <div className="wrapper flex-between">
         <div className="flex-start">
-          <Link href="/" className="flex-start">
+          <Link href="/" className="flex-start ml-4">
+            <CategoryDrawer />
             <Image
               src="/logo.svg"
               alt={`${APP_NAME} logo`}
